f251172a0bc90b228ce759604612ab0aafbdcd88
Freitas65 <gabrielfritas77@gmail.com>
2026-04-01 17:06:38 -0400

    test

---
+++ Exercicio3.py
@@ -0,0 +1,4 @@
+Faltas_1bimestre = int(input("Digite as faltas do 1 Bimestre: "))
+Faltas_2bimestre = int(input("Digite as faltas do 2 Bimestre: 1"))
+total_faltas = Faltas_1bimestre + Faltas_2bimestre
+print ("O total de faltas é:", total_faltas)
