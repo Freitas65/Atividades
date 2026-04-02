7c95b73d0af1a824381e68db6bf146aa7484ff18
Freitas65 <gabrielfritas77@gmail.com>
2026-04-01 20:57:24 -0400

    Atividades

---
+++ Exercicio8.py
@@ -0,0 +1,5 @@
+cateto1 = float(input("Digite o valor do primeiro lado: "))
+cateto2 = float(input("Digite o valor do segundo lado: "))
+soma_quadrados = (cateto1 ** 2)
+hipotenusa = soma_quadrados **0.5
+print("A hipotenusa é", hipotenusa)
